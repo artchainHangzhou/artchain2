be3754b8744c0ad35a91e8496a47e3f9a3b069b2
zjb0807 <zjb0807@qq.com>
2017-05-13 23:58:51 +0800

    fix search

--- artservice/search.go
+++ artservice/search.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+    "fmt"
+    "bytes"
     "net/http"
+    "io/ioutil"
+    "mime/multipart"
 )
 
 func Search(w http.ResponseWriter, r *http.Request) {
@@ -16,17 +20,47 @@ func Search(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+	r.ParseMultipartForm(32 << 20)
+	file, handler, err := r.FormFile("uploadfile")
+	if err != nil {
+		fmt.Println(err)
+		OutputJson(w, -1, err.Error(), nil)
+		return
+	}
+	defer file.Close()
+    fd, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+		OutputJson(w, -1, err.Error(), nil)
+		return
+	}
 
-    if r.PostFormValue("fileStyle") == "" {
-        OutputJson(w, -1, "fileStyle is null", nil)
-        return
+	var buff bytes.Buffer
+	writer := multipart.NewWriter(&buff)
+	req, _ := writer.CreateFormFile("file", handler.Filename)
+	req.Write(fd)
+	writer.Close()
+	var client http.Client
+	resp, err := client.Post(apiimages, writer.FormDataContentType(), &buff)
+	if err != nil {
+		fmt.Println("search Post fail:", err.Error())
+		OutputJson(w, -1, err.Error(), nil)
+        return 
+	}
+	defer resp.Body.Close()
+	data, _ := ioutil.ReadAll(resp.Body)
+	fileStyle := string(data)
+	fmt.Println(fileStyle)
+
+    if fileStyle == "" {
+		OutputJson(w, -1, "fileStyle is null", nil)
+		return
     }
-	
 
 	var args []string
 	args = append(args, "invoke")
 	args = append(args, "search")
-	args = append(args, r.PostFormValue("fileStyle"))
+	args = append(args, fileStyle)
 
 	value, err := base.Query(base.ChainID, base.ChainCodeID, args)
 	if err != nil {
--- fabric/rm.sh
+++ fabric/rm.sh
@@ -2,4 +2,4 @@ docker kill $(docker ps -qa)
 docker rm $(docker ps -qa)
 docker network rm $(docker network ls -q)
 docker rmi $(docker images | grep "\dev-peer[0-9]*\-" | tr -s ' ' | cut -d ' ' -f 1 | less)
-rm -rf /var/hyperledger/couchdb*
+#rm -rf /var/hyperledger/
